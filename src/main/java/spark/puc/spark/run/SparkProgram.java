b4ef1bcbafd5d78600ba3fd8cd07a1518261d648
santospadua <antonio.edu.oracle@gmail.com>
2021-01-10 00:14:09 -0300

    First RDD about crimes by year

---
+++ src/main/java/spark/puc/spark/run/SparkProgram.java
@@ -0,0 +1,30 @@
+package spark.puc.spark.run;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class SparkProgram 
+{
+	public static void main(String[] args)
+	{
+		SparkConf config = new SparkConf().setMaster("local[*]").setAppName("ATP");
+        
+        try (JavaSparkContext context = new JavaSparkContext(config))
+        {
+            JavaRDD<String> file = context.textFile("files/ocorrencias_criminais.csv");
+            
+            JavaPairRDD<String, Integer> crimesPorAno = file.mapToPair(x ->
+            {
+                 String[] lines = x.split(";");
+                 return new Tuple2<>(lines[2], 1);
+            }).reduceByKey((x, y) -> x + y);
+            
+            System.out.println(crimesPorAno.collect());
+            crimesPorAno.saveAsTextFile("crimesPorAno");
+        }
+	} 
+}
