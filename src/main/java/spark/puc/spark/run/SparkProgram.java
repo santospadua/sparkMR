0deab1e5ca5361d029101ef68f16cddb4c100faf
santospadua <antonio.edu.oracle@gmail.com>
2021-01-11 23:53:20 -0300

    Other operations with JavaPairRDD

--- src/main/java/spark/puc/spark/run/SparkProgram.java
+++ src/main/java/spark/puc/spark/run/SparkProgram.java
@@ -9,22 +9,41 @@ import scala.Tuple2;
 
 public class SparkProgram 
 {
+    private static JavaSparkContext context;
+    
     public static void main(String[] args)
     {
         SparkConf config = new SparkConf().setMaster("local[*]").setAppName("ATP");
         
-        try(JavaSparkContext context = new JavaSparkContext(config))
+        context = new JavaSparkContext(config);
+        
+        JavaRDD<String> file = context.textFile("files/ocorrencias_criminais.csv");
+            
+        JavaPairRDD<String, Integer> crimesPorAno = file.mapToPair(x ->
         {
-            JavaRDD<String> file = context.textFile("files/ocorrencias_criminais.csv");
+            String[] lines = x.split(";");
+            return new Tuple2<>(lines[2], 1);
+        }).reduceByKey((x, y) -> x + y);
             
-            JavaPairRDD<String, Integer> crimesPorAno = file.mapToPair(x ->
-            {
-                String[] lines = x.split(";");
-                return new Tuple2<>(lines[2], 1);
-            }).reduceByKey((x, y) -> x + y);
-            
-            System.out.println(crimesPorAno.collect());
-            crimesPorAno.saveAsTextFile("crimesPorAno");
-        }
+        System.out.println(crimesPorAno.collect());
+        crimesPorAno.saveAsTextFile("crimesPorAno");
+        
+        JavaPairRDD<Tuple2<String, String>, Integer> crimesPorAnoNarcoticos = file.mapToPair(t -> 
+        {    
+            String[] lines = t.split(";");
+            return new Tuple2<>(new Tuple2<>(lines[2], lines[4]), 1); 
+        }).filter(f -> f._1()._2().equals("NARCOTICS")).reduceByKey((x, y) -> x + y);
+        
+        System.out.println(crimesPorAnoNarcoticos.collect());
+        crimesPorAnoNarcoticos.saveAsTextFile("crimesPorAnoNarcoticos");
+        
+        JavaPairRDD<Tuple2<String, String>, Integer> crimesDiasParesNarcoticos = file.mapToPair(x -> 
+        {
+            String[] lines = x.split(";");
+            return new Tuple2<>(new Tuple2<>(lines[2], lines[4]), Integer.parseInt(lines[0]));
+        }).filter(f -> f._2() % 2 == 0 && f._1()._2().equals("NARCOTICS")).reduceByKey((x, y) -> x + y);
+        
+        System.out.println(crimesDiasParesNarcoticos.collect());
+        crimesPorAnoNarcoticos.saveAsTextFile("crimesDiasParesNarcoticos");
     }
 }
